8237baf7143317fc2b18845a1495c20360663eb5
Erié <erie31@gmail.com>
2026-02-28 18:08:12 -0300

    readme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eeriescore-v1';
+const CACHE_NAME = 'eeriescore-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -8,6 +8,18 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
 
